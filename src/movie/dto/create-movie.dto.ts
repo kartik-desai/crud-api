1d09c609b433c98d63a4ac156153662cf62236de
Kartik Desai <desaikartik99@gmail.com>
2023-06-06 11:16:17 +0530

    feat: added pipes for validation

--- src/movie/dto/create-movie.dto.ts
+++ src/movie/dto/create-movie.dto.ts
@@ -1,13 +1,24 @@
 import { Genre } from '../schemas/movie.schema';
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateMovieDto {
+  @IsNotEmpty()
+  @IsString()
   readonly title: string;
 
+  @IsNotEmpty()
+  @IsString()
   readonly plot: string;
 
+  @IsNotEmpty()
+  @IsString()
   readonly director: string;
 
+  @IsNotEmpty()
+  @IsNumber()
   readonly rating: number;
 
+  @IsNotEmpty()
+  @IsEnum(Genre, { message: 'Please enter correct genre.' })
   readonly genre: Genre;
 }
--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,13 +1,24 @@
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
 import { Genre } from '../schemas/movie.schema';
 
 export class UpdateMovieDto {
+  @IsOptional()
+  @IsString()
   readonly title: string;
 
+  @IsOptional()
+  @IsString()
   readonly plot: string;
 
+  @IsOptional()
+  @IsString()
   readonly director: string;
 
+  @IsOptional()
+  @IsNumber()
   readonly rating: number;
 
+  @IsOptional()
+  @IsEnum(Genre, { message: 'Please enter correct genre.' })
   readonly genre: Genre;
 }
